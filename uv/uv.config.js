ebe4f30586a48406d8a35bec490d3917c26505e8
CAP <159663130+8bitCAP@users.noreply.github.com>
2025-05-22 10:53:25 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/embed/',
     
     // BARE SERVER
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    bare:'wss://deploy-wisp.vercel.app/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
