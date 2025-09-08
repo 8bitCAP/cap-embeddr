65521d8e36837c156542a3d1812ec9de3fe6fd9b
CAP <159663130+8bitCAP@users.noreply.github.com>
2025-09-08 18:57:41 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,9 +1,8 @@
 self.__uv$config = {
-    prefix: '/static/petezah/',
+    prefix: '/embed/',
     
     // BARE SERVER https://bare.petezahgames.com/  https://petezahserver.faizinternational.com.np/bare/ 
-    // bare:'https://petezahgames.com/bare/',
-    bare:'cap-bare.vercel.app/bare/',
+    bare:'https://petezahgames.com/bare/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
