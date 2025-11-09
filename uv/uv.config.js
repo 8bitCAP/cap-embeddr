c844c74ebfdb42dcca9713384d856a3afc469b5d
CAP <159663130+8bitCAP@users.noreply.github.com>
2025-11-08 18:00:14 -0600

    Update bare server URL in uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/embed/',
     
     // BARE SERVER https://bare.petezahgames.com/  https://petezahserver.faizinternational.com.np/bare/ 
-    bare:'https://petezahgames.com/bare/',
+    bare:'https://gointerstellar.app/ca/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
